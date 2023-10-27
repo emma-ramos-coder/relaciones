14848843f92450bf73c4559972fa0e448c91cd8e
Emma Ramos <emmyramosc@gmail.com>
2023-10-27 00:57:50 -0500

    relacion uno a uno entre User y Phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Phone;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // definición de la relación con phone
+    public function phone(): hasOne{
+        return $this->hasOne(Phone::class);
+    }
 }
